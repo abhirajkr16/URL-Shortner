8db4a42378da888e3f156549c443e42f59d17e30
Abhiraj kumar <abhirajmait16@gmail.com>
2026-07-13 11:46:50 +0530

    :feat: login functionality with JWT authentication

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,6 +5,8 @@ import UnauthorizedError from "../errors/UnauthorizedError.js";
 import NotFoundError from "../errors/NotFoundError.js";
 
 import { validateRegisterData } from "../validators/auth.validator.js";
+import { generateAccessToken } from "../utils/jwt.js";
+import { validateLoginData } from "../validators/auth.validator.js";
 
 import {
     findUserByEmail,
@@ -45,3 +47,28 @@ export const registerUser = async ({
     // Fetch and return the created user
     return findUserById(userId);
 };
+
+export const loginUser = async ({ email, password }) => {
+    validateLoginData({ email, password });
+    const user = await findUserByEmail(email);
+    if (!user) {
+        throw new UnauthorizedError("Invalid credentials");
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordCorrect) {
+        throw new UnauthorizedError("Invalid credentials");
+    }
+    const token = generateAccessToken({ userId: user.id, email: user.email });
+    return {
+        token,
+        user: {
+            id: user.id,
+            username: user.username,
+            email: user.email
+        },
+    };
+
+
+
+}
